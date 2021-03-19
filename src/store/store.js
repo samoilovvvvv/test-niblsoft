6255b26a25c00db2c35ab9197a261f85f8073e66
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-19 23:09:48 +0300

    feat: add writing to file

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,43 @@
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import note from '../json/note.json'
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from 'redux-thunk'
 
 const add = 'ADD_NOTE'
 
 const reducer = (state = note, action) => {
   switch (action.type) {
     case add:
-      break;
+      return {
+        ...state,
+        note: action.payload
+      }
     default:
       return state
   }
 }
 
-export const store = createStore(reducer)
+export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export const addNote = (name, tags, text) => {
-  return {
-    type: add,
-    name,
-    tags,
-    text
+  return async dispatch => {
+    const noteObj = {
+      id: note.note.length + 1,
+      name,
+      tags: tags.split(' '),
+      text
+    }
+  
+    note.note.push(noteObj)
+    
+    await fetch('http://localhost:3001/api/update_json', {
+      method: 'POST',
+      body: JSON.stringify(note)
+    })
+    
+    dispatch({
+      type: add,
+      payload: note.note
+    })
   }
 }
